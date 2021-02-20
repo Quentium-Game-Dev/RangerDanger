34d0024fcd56f7d258ad5cb35e264d8fb651f41d
JWorthington96 <44651791+JWorthington96@users.noreply.github.com>
2021-02-20 17:38:23 +0000

    added start of world generation

---
+++ Ranger Danger/Assets/WorldGeneration/WorldChunk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldChunk : MonoBehaviour
+{
+    // Various example properties for now
+    public int width;
+    public int height;
+    public Sprite sprite;
+
+    public List<Vector2> enemySpawnPoints;
+    public List<GameObject> enemyPrefabs;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // method to spawn in enemies
+    public void SpawnEnemy()
+    {
+
+    }
+}
---
+++ Ranger Danger/Assets/WorldGeneration/WorldGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public List<WorldChunk> allWorldChunks;
+    public Queue<WorldChunk> worldChunks;
+    public Stack<WorldChunk> previousWorldChunks;
+
+    public WorldChunk currentChunk;
+    public WorldChunk nextChunk;
+    public EventChunk eventChunk;
+
+    public double probOfPrevChunk;
+    public double probOfRepeatChunk;
+
+    public float eventChunkSlope = 0.75f;
+    public float eventChunkMidPoint = 10; 
+
+    public int seed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random rand = new Random(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateQueue(Random rand)
+    {
+        worldChunks = new Queue<WorldChunk>();
+
+        var queueLength = 20;
+        for (int i = 0; i < queueLength; i++)
+        {
+            worldChunks.Enqueue(allWorldChunks[rand.Next(allWorldChunks.Count)]);
+        }
+    }
+
+    // method to run whenever the player steps on a different chunk
+    void UpdateWorldChunk()
+    {
+        currentChunk = nextChunk;
+    }
+
+    // method to run whenever the previous block has left the camera
+    void UpdateNextWorldChunk()
+    {
+        Random rand = new Random(seed);
+        
+        if (rand.NextDouble() < ProbabilityOfEventChunk())
+        {
+            nextChunk = eventChunk;
+            return;
+        }
+
+        if (rand.NextDouble() < probOfRepeatChunk)
+        {
+            nextChunk = currentChunk;
+            return;
+        }
+
+        if (rand.NextDouble() < probOfPrevChunk)
+        {
+            nextChunk = previousWorldChunks.Pop();
+            return;
+        }
+
+        previousWorldChunks.Push(currentChunk);
+        nextChunk = worldChunks.Dequeue();
+    }
+
+    double ProbabilityOfEventChunk()
+    {
+        return 1 / (1 + Mathf.Exp(-eventChunkSlope * (previousWorldChunks.Count - eventChunkMidPoint)));
+    }
+}

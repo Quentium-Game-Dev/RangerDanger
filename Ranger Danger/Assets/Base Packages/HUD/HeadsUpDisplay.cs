ce7f5ffe0658599e40d584d555465b26929405a0
Sami Zirak <samizirakdev@gmail.com>
2021-03-12 17:17:13 +0000

    player - Stamina Bar UI
    Implemented a UI Canvas that will serve as the HUD for the player and implemented a script that displays the players current stamina at runtime through a UI slider.

---
+++ Ranger Danger/Assets/Base Packages/HUD/HeadsUpDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadsUpDisplay : MonoBehaviour
+{
+    public Slider staminaBar;
+    public GameObject player;
+
+    private float maxStamina;
+    private float currentStamina;
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxStamina = player.GetComponent<Player>().maximumStamina;
+        currentStamina = player.GetComponent<Player>().availableStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = currentStamina;
+    }
+}
